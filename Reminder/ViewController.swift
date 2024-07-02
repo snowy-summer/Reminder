de2922fe4ccba5ad2f21282c8196e4f3f79bf86b
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-07-02 16:03:13 +0900

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by 최승범 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
